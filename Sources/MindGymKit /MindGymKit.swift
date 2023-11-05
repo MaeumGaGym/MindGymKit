36868e801c1be7cfbed7e64bb1dcaa6e742507f4
junha <goodjunha@gmail.com>
2023-11-05 21:28:23 +0900

    ⚓️:: #3 mindGymKit 추가

--- Sources/MindGymKit /MindGymKit.swift
+++ Sources/MindGymKit /MindGymKit.swift
@@ -1,3 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import UIKit
+
+open class MindGymKit {
+    var helloMind: String = "마음가짐"
+    open var publicHelloMind: String = "안녕 마음가짐"
+    
+    public init(helloMind: String) {
+        publicHelloMind = helloMind
+    }
+}
