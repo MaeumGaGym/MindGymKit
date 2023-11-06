4133debc214779d30d2461e387300ac5192bc7a5
junha <goodjunha@gmail.com>
2023-11-06 16:25:19 +0900

    ⚓️:: [#12] 오늘의 만보기 가져오기

--- Sources/MindGymKit /MindGymKit.swift
+++ Sources/MindGymKit /MindGymKit.swift
@@ -1,13 +1,42 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import UIKit
+import HealthKit
 
-open class MindGymKit {
-    var helloMind: String = "마음가짐"
-    open var publicHelloMind: String = "안녕 마음가짐"
+public class MindGymKit {
+    public init() {}
     
-    public init(helloMind: String) {
-        publicHelloMind = helloMind
+    public func fetchTodayStepCount(completion: @escaping (Double?) -> Void) {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            completion(nil)
+            return
+        }
+        
+        let healthStore = HKHealthStore()
+        let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        let calendar = Calendar.current
+        let now = Date()
+        let startOfDay = calendar.startOfDay(for: now)
+        let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: endOfDay, options: .strictStartDate)
+        
+        healthStore.requestAuthorization(toShare: nil, read: [stepType]) { (success, error) in
+            if success {
+                let query = HKStatisticsQuery(quantityType: stepType, quantitySamplePredicate: predicate, options: .cumulativeSum) { query, result, error in
+                    if let result = result, let sum = result.sumQuantity() {
+                        completion(sum.doubleValue(for: HKUnit.count()))
+                    } else {
+                        completion(nil)
+                        if let error = error {
+                            print("걸음 수 가져오기 오류: \(error.localizedDescription)")
+                        }
+                    }
+                }
+                healthStore.execute(query)
+            } else {
+                completion(nil)
+                if let error = error {
+                    print("HealthKit 권한 요청 중 오류 발생: \(error.localizedDescription)")
+                }
+            }
+        }
     }
 }
