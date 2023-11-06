80b694e828f99327b62cea5ce89125d0191209bc
junha <goodjunha@gmail.com>
2023-11-06 15:39:51 +0900

    🎮:: [#4] 테스트 파일 추가

---
+++ EXMindGymKit/EXMindGymKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EXMindGymKit
+//
+//  Created by 박준하 on 11/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
